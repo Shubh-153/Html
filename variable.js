88c9436fd071ba46f3eb5e87912c3fe941b46d52
Shubh Gupta <shubhgupta194@gmail.com>
2026-05-05 10:40:17 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,17 @@
+let x=12;
+let y=13;
+console.log(x,y);
+console.log(x+" "+y)
+console.log(`${x} ${y}`);
+{
+    let x1=12;
+    let y1=13;
+    x1 =25;
+    console.log(x1);
+}
+let a=12.34;
+let b=34.99;
+console.log(a+b);
+let string1="ABC"
+let string2="PQR"
+console.log(string1+" "+string2)
